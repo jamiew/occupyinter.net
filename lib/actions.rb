8a56d1655c6aef894443c7c497efe571716ce844
Jamie Wilkinson <jamie@jamiedubs.com>
2011-10-09 18:54:27 -0700

    DRY up action responses, fix URL logic

--- lib/actions.rb
+++ lib/actions.rb
@@ -1,27 +1,34 @@
+# PUT /count.json?url=...
 put "/count" do
 
   return make_error("Missing UUID") if params[:uuid].blank?
   @site = get_site(params[:url])
   @visit = Visit.first(:uuid => params[:uuid], :site_id => @site.id) rescue nil
-  
+
   if @visit.blank?
     @visit = Visit.create(:uuid => params[:uuid], :site_id => @site.id)
     @site.visits_count += 1
     @site.save
   end
 
-  respond_to do |format|
-    format.json { make_json({:site => @site.domain, :visits => @site.visits_count, :created_at => @site.created_at, :updated_at => @site.updated_at}) }
-    format.html { haml :'api/site' }
-  end
+  respond_with_stats(@site)
 end
 
-
+# GET /site.json?url=...
 get "/site" do
+  return make_error("You must specify a ?url param") if params[:url].blank?
+
   @site = get_site(params[:url])
+  return make_error("Invalid site URL") if @site.blank?
+
+  respond_with_stats(@site)
+end
+
+def respond_with_stats(site)
+  output = {:site => site.domain, :visits => site.visits_count, :created_at => site.created_at, :updated_at => site.updated_at}
 
   respond_to do |format|
-    format.json { make_json({:site => @site.domain, :visits => @site.visits_count, :created_at => @site.created_at, :updated_at => @site.updated_at}) }
+    format.json { make_json(output) }
     format.html { haml :'api/site' }
   end
 end
@@ -35,5 +42,5 @@ end
 
 
 get "/" do
-  redirect "http://occupyinter.net"
+  # redirect "http://occupyinter.net"
 end
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,16 +1,17 @@
 helpers do
 
-  def dev?; (Sinatra::Application.environment.to_s != 'production'); end
+  def dev?
+    Sinatra::Application.environment.to_s != 'production'
+  end
 
   def get_site(url)
     url = url.gsub(/^(.*)(\:\/\/)([A-Z0-9\-\_\.\:]+)(\/.*)$/i, '\3') rescue nil
-    return make_error("Invalid site URL") if url.blank? || url.match(/\//)
-    
+    return nil if url.blank? || url.match(/\//)
+
     site = Site.find_by_domain(url) rescue nil
     site ||= Site.create(:domain => url)
     return site
   end
-
 
   def make_error(err=nil)
     err ||= 'Unknown Error'
