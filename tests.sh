59d8d6d731e0e64ee95626b2deec87624df5e54a
Jamie Wilkinson <jamie@jamiedubs.com>
2011-10-12 22:11:32 -0700

    Pimp my tests

--- tests.sh
+++ tests.sh
@@ -8,22 +8,24 @@ uuid="REPLACEME"
 ###
 
 echo; echo " -- Joining site: $url"
-curl -X PUT -d lol "http://$server/count.json?url=$url&uuid=$uuid"; echo
+curl "http://$server/join_protest.json?url=$url&uuid=$uuid"; echo
 
 echo; echo " -- Joining site: www.$url"
-curl -X PUT -d lol "http://$server/count.json?url=www.$url&uuid=$uuid"; echo
+curl "http://$server/join_protest.json?url=www.$url&uuid=$uuid"; echo
 
-echo; echo " -- Joining site: http://$url"
-curl -X PUT -d lol "http://$server/count.json?url=http://$url&uuid=$uuid"; echo
+echo; echo " -- Joining site: http://$url with diff useragent"
+curl -A foobar "http://$server/join_protest.json?url=http://$url&uuid=$uuid"; echo
 
-echo; echo " -- Joining site: http://www.$url"
-curl -X PUT -d lol "http://$server/count.json?url=http://www.$url&uuid=$uuid"; echo
+echo; echo " -- Joining site: http://www.$url with diff useragent"
+curl -A foobar2 "http://$server/join_protest.json?url=http://www.$url&uuid=$uuid"; echo
 
 ###
 
-echo; echo " -- Get site stats --"
-curl "http://$server/site.json?url=$url"; echo
+echo; echo " -- Get site stats"
+curl "http://$server/stats.json?url=$url"; echo
 
+echo; echo " -- Site stats protestor count should == 3"
+curl "http://$server/stats.json?url=$url" | python -mjson.tool | grep protestor_count
 
 
 
