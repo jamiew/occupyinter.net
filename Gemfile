6fff05559e0b34f89f6ef74264d836453bce4f0e
Jamie Wilkinson <jamie@jamiedubs.com>
2011-10-18 11:32:48 -0700

    Remove tons of stuff. We just want an embed

--- Gemfile
+++ Gemfile
@@ -1,35 +1,16 @@
 source :rubygems
 
-gem "sinatra",              "1.2.6"
-gem "configatron",          "2.8.0"
-gem "haml",                 "3.1.2"
-gem "json",                 "1.4.6"
+gem "sinatra", "1.2.6"
+gem "haml", "3.1.2"
+gem "json", "1.4.6"
 gem "sinatra-respond_to",   "0.7.0"
-gem "sinatra-redis"
-
-gem "data_objects",         "0.10.2"
-gem "dm-core",              "0.10.2"
-gem "dm-aggregates",        "0.10.2"
-gem "dm-ar-finders",        "0.10.2"
-gem "dm-timestamps",        "0.10.2"
-gem "dm-types",             "0.10.2"
-gem "dm-validations",       "0.10.2"
-
-group :mysql do
-  gem "do_mysql", "0.10.2"
-end
-
-group :postgres do
-  gem "do_postgres", "0.10.2"
-end
 
 group :development, :test do
-  gem "do_sqlite3"
   gem "shotgun"
   gem "wirble"
 end
 
 group :test do
-  gem "rspec",              "2.6.0"
-  gem "rack-test",          "0.6.0"
+  gem "rspec", "2.6.0"
+  gem "rack-test", "0.6.0"
 end
--- app.rb
+++ app.rb
@@ -1,31 +1,25 @@
 require 'rubygems'
 require 'sinatra'
-require 'sinatra/redis'
 require 'digest/sha1'
 
 configure do |config|
   set :sessions, true
 
-  # TODO namespace all redis calls, or pick a genuinely unique DB number
-  set :redis, ENV['REDISTOGO_URL'] || 'redis://localhost:6379/1'
+  %w(haml sinatra/respond_to json).each{|lib| require lib}
 
-  ROOT = File.expand_path(File.dirname(__FILE__))
-
-  %w(haml sinatra/respond_to configatron json dm-core dm-types dm-timestamps dm-aggregates dm-ar-finders dm-validations).each{|lib| require lib}
-
-  Rack::MethodOverride # FOR PUT METHOD
   Sinatra::Application.register Sinatra::RespondTo
+  # configatron.configure_from_yaml("#{settings.root}/settings.yml", :hash => Sinatra::Application.environment.to_s)
 
-  configatron.configure_from_yaml("#{ROOT}/settings.yml", :hash => Sinatra::Application.environment.to_s)
-
-  %w(lib/helpers lib/actions models/site models/visit models/user).each{|lib| require "#{ROOT}/#{lib}"}
-  # DataMapper::Logger.new(STDOUT, :debug) unless ENV['RACK_ENV'] == 'production'
-  DataMapper.setup(:default, ENV["DATABASE_URL"] || configatron.db_connection.gsub(/ROOT/, ROOT))
-  DataMapper.auto_upgrade!
+  %w(helpers).each{|lib| require "#{settings.root}/#{lib}"}
 end
 
+# Global
+before do
+  puts
+  puts "*** [#{Time.now.strftime('%m-%d-%Y %h:%m:%ms')}] #{request.url} #{params.inspect}"
+end
 
-# 404 errors
+# 404s
 not_found do
   @title ||= '404 File Not Found'
   @error ||= 'Sorry, but the page you were looking for could not be found.'
@@ -36,16 +30,26 @@ not_found do
   end
 end
 
-# 500 errors
+# 500s
 error do
   err = request.env['sinatra.error'].message
   err ||= 'An unknown error has occured.'
   make_error(error)
 end
 
+# -------------------------
 
-# Require before
-before do
-  puts
-  puts "*** [#{Time.now.strftime('%m-%d-%Y %h:%m:%ms')}] #{request.url} #{params.inspect}"
+get "/" do
+  respond_to do |format|
+    format.html { haml :frontpage }
+  end
+end
+
+get "/embed" do
+  content_type :html # so it renders widget.html, :format => :html does not work. WTF such a hack
+  widget = erb :widget
+  content_type :js
+  respond_to do |format|
+    format.js { "document.write(#{widget.to_json})" }
+  end
 end
